bf0319822cf32413c884b330cee5b48d99933d37
Alexander <sapfirmoscow@yandex.ru>
2018-12-02 12:50:04 +0300

    Initial commit

---
+++ app/src/main/java/ru/sberbank/homework1/SplashActivity.java
@@ -0,0 +1,18 @@
+package ru.sberbank.homework1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Intent intent =  new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
